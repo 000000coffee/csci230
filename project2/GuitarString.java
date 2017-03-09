3898809407edadff187b2f8dbdd17dd1f9359264
000000coffee <kottwitz.matt@gmail.com>
2017-03-09 14:16:02 -0800

    constructor

--- project2/GuitarString.java
+++ project2/GuitarString.java
@@ -1,6 +1,13 @@
 public class GuitarString {
+   private static final double SAMPLE_RATE = 44100;
+   private double frequency; // sampling rate per second
+   private RingBuffer string;
    public GuitarString(double frequency) {
-   
+      int n = ((int)(Math.ceil(SAMPLE_RATE / (double)frequency)));
+      string = new RingBuffer(n);
+      for (int i = 0; i < n; ++i) {
+         
+      }
    }
    public GuitarString(double[] init) {
    
