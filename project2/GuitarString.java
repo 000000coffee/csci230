d026593c508bb24e419834b725ae46801f5c0af4
000000coffee <kottwitz.matt@gmail.com>
2017-03-14 12:05:38 -0700

    added comments/clean-up methods

--- project2/GuitarString.java
+++ project2/GuitarString.java
@@ -3,42 +3,48 @@ import java.util.Random;
 public class GuitarString {
    private static final int SAMPLE_RATE = 44100;
    private static final double ENERGY_DECAY_FACTOR = 0.994;
-   private double frequency; // sampling rate per second
-   private int times;
-   private RingBuffer string;
+   private double frequency;  // sampling rate per second
+   private int tics;         // times tic() is called
+   private RingBuffer string; // ring buffer imitating a guitarString
+   /*    compiler ~ frequency to fill a ring buffer of size with all 0's   */
    public GuitarString(double frequency) {
-      times = 0;
+      tics = 0;
       int n = ((int)(Math.ceil(((double)SAMPLE_RATE) / frequency)));
       string = new RingBuffer(n);
       for (int i = 0; i < n; ++i) {
          string.enqueue(0.00);
       }
    }
+   /*    compiler ~ known samples to initialize the string with   */
    public GuitarString(double[] init) {
-      times = 0;
+      tics = 0;
       for (int i = 0; i < init.length; ++i) {
          string.enqueue(init[i]);
       }
    }
+   /*    pluck the string ~ input random values to ring buffer    */
    public void pluck() {
       Random rand = new Random();
       for (int i = string.getFirst(); i < string.size(); ++i) {
-         // first dequeue to make space
-         string.dequeue();
-         // then enqueue
-         string.enqueue(rand.nextDouble() - 0.5);
+         double r = rand.nextDouble() - 0.5; // rand between -0.5 and +0.5
+         string.dequeue();                   // first dequeue to make space
+         string.enqueue(r);                  // then enqueue our rand
       }
    }
+   /*    advance the string one tic by applying the Karplus-Strong update  */
    public void tic() {
-      double tmp = string.dequeue();
-      double next = ENERGY_DECAY_FACTOR * 0.5 * (string.peek() + tmp);
-      ++times;
-      string.enqueue(next);
+      double tmp = string.dequeue();            // remove front
+      double avg = 0.5 * (string.peek() + tmp); // average of first two items
+      double next = ENERGY_DECAY_FACTOR * avg;  // compute next
+      string.enqueue(next);                     // enqueue next
+      ++tics;                                   // increment tics
    }
+   /*    return front sample of string    */
    public double sample() {
       return string.peek();
    }
+   /*    number of times tic was called   */
    public int time() {
-      return times;
+      return tics;
    }
 }
